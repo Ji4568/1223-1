ae0c65c2e6f661d27f92a2b8511ef2c71cebd45c
呂佶 <ji8058882@gmail.com>
2023-12-21 22:19:26 +0800

    熟悉R裡面的基本物件(1)

---
+++ 3.R
@@ -0,0 +1,10 @@
+#我們正式介紹一下物件，在R裡面物件有相當多屬性，而剛剛那兩個物件屬於同個種類叫做「向量(vector)」，在R裡面大致上將物件分為幾個層次，主要共分3個層次：
+#變數(Variable)層：邏輯(logical)向量、整數(integer)向量、因子(factor)向量、數字(numeric)向量、文字(character)向量
+
+#陣列(Array)層：矩陣(matrix)、資料表(data.frame)
+
+#列表(List)層：列表(list)、S3物件(S3 class)、S4物件(S4 class)
+
+
+
+
